b46282fe63de1bd60bcecb3afca5f6368b26d084
Pawel Sawicki <public@psawicki.de>
2022-09-29 12:04:08 +0200

    +ngOnDestroy

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Book } from './book';
 import { BookApiService } from './book-api.service';
 
@@ -7,7 +8,9 @@ import { BookApiService } from './book-api.service';
   templateUrl: './book.component.html',
   styleUrls: ['./book.component.scss'],
 })
-export class BookComponent implements OnInit {
+export class BookComponent implements OnInit, OnDestroy {
+  private _sub = new Subscription();
+
   constructor(private readonly _books: BookApiService) {}
 
   books: Book[] | null = null;
@@ -23,10 +26,16 @@ export class BookComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this._books.getAll().subscribe({
+    const sub = this._books.getAll().subscribe({
       next: (books) => {
         this.books = books;
       },
     });
+
+    this._sub.add(sub);
+  }
+
+  ngOnDestroy(): void {
+    this._sub.unsubscribe();
   }
 }
